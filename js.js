a587c1bb8c3a04194d8cbf81ac0a4f738573e4be
Buks <abolarinbukolastella@gmail.com>
2021-02-27 03:08:03 +0100

    done v2

--- js.js
+++ js.js
@@ -7,18 +7,22 @@ const screen = document.querySelector(".screen");
 const equals = document.querySelector(".equals");
 const resultBox = document.querySelector(".result");
 const workings = document.querySelector(".workings");
+
+//
+btn.querySelectorAll("div").forEach((ev) => {
+  ev.classList.add("light");
+});
 dark.addEventListener("click", function () {
   dark.classList.toggle("not-active");
   light.classList.toggle("not-active");
   container.style.backgroundColor = "#292d36";
   container.style.color = "white";
   //   console.log(btn.querySelectorAll("div")[0]);
-  btn
-    .querySelectorAll("div")
-    .forEach(
-      (ev) =>
-        (ev.style.boxShadow = "4px 4px 5px #181b20, -1px -1px 5px #24282c")
-    );
+  btn.querySelectorAll("div").forEach((ev) => {
+    ev.style.boxShadow = "4px 4px 5px #181b20, -1px -1px 5px #24282c";
+    ev.classList.remove("light");
+    ev.classList.add("dark");
+  });
   screen.style.boxShadow =
     "inset 6px 6px 6px  #181b20 , inset -6px -3px 6px #777777";
 });
@@ -28,11 +32,11 @@ light.addEventListener("click", function () {
   container.style.backgroundColor = "#ebeff2";
   container.style.color = "black";
   //   console.log(btn.querySelectorAll("div")[0]);
-  btn
-    .querySelectorAll("div")
-    .forEach(
-      (ev) => (ev.style.boxShadow = "5px 5px 5px #cbced1, -5px -5px 10px white")
-    );
+  btn.querySelectorAll("div").forEach((ev) => {
+    ev.style.boxShadow = "5px 5px 5px #cbced1, -5px -5px 10px white";
+    ev.classList.add("light");
+    ev.classList.remove("dark");
+  });
   screen.style.boxShadow =
     "inset 6px 6px 6px #cbced1, inset -6px -6px 6px white";
 });
@@ -79,51 +83,59 @@ const calculator = function (exp) {
     }
   }
 };
-
 let box1 = "";
 let box2 = "";
 let operator = "";
 let counter = 0; //to let us know whether we are just starting the operation
 let index = 0;
 let clicked = 0;
-
-workings.textContent = "";
-resultBox.textContent = "";
+function clearAll() {
+  box1 = "";
+  box2 = "";
+  operator = "";
+  counter = 0; //to  us know whether we are just starting the operation
+  index = 0;
+  clicked = 0;
+  workings.textContent = "";
+  resultBox.textContent = "";
+}
+const animateDown = function (resultBox) {
+  //bring it down
+  resultBox.style.transform = "translateY(0px)";
+  resultBox.style.fontSize = "1.5rem";
+  resultBox.style.transition = "transform 0s ease-in-out";
+};
+clearAll();
 btn.addEventListener("click", function (e) {
   //guard clause
   if (e.target.classList.contains("btn")) return;
   //operators
   if (e.target.classList.contains("n") || e.target.classList.contains("o")) {
     if (e.target.textContent == "AC") {
-      box2 = box1 = "";
-      let operator = "";
-      let index = 0;
-      let clicked = 0;
-      counter = 0;
-      workings.textContent = "";
-      resultBox.textContent = "";
+      clearAll();
       return;
     }
     operator = e.target.textContent;
     if (workings.textContent == "") {
-      //bring it down
-      resultBox.style.transform = "translateY(0px)";
-      resultBox.style.fontSize = "1.5rem";
-      resultBox.style.transition = "transform 0s ease-in-out";
+      animateDown(resultBox);
       //as if you are continuing the operation
       workings.textContent = box1 = resultBox.textContent;
-      resultBox.textContent = "";
-      box2 = "";
+      resultBox.textContent = box2 = "";
     }
     //plus or minus sign
     if (e.target.textContent == "⁺⁄₋") {
       box2 = resultBox.textContent;
-      if (!box2) box2 = workings.textContent;
+      if (!box2) {
+        box2 = workings.textContent;
+        if (!Number(box2)) {
+          clearAll();
+          return;
+        }
+      }
       let n = box2;
       n = Number(n) * -1;
       box2 = String(n);
-      box1 = box2;
-      workings.textContent = box2;
+      box1 = workings.textContent = box2;
       box2 = "";
       counter = 0;
       return;
@@ -176,15 +188,10 @@ btn.addEventListener("click", function (e) {
   }
   //if what you press after the result button is a number
   if (resultBox.style.transform == "translateY(-30px)") {
-    //bring it down
-    resultBox.style.transform = "translateY(0px)";
-    resultBox.style.fontSize = "1.5rem";
+    animateDown();
     //clear all
     //similar to as if you are starting again
-    workings.textContent = "";
-    box1 = "";
-    box2 = "";
-    resultBox.textContent = "";
+    workings.textContent = box1 = box2 = resultBox.textContent = "";
   }
   if (counter == 0) {
     box1 += e.target.textContent;
@@ -197,13 +204,7 @@ btn.addEventListener("click", function (e) {
     const data = box1 + operator + box2;
     const result = calculator(data);
     if (!result && result != 0) {
-      box2 = box1 = "";
-      let operator = "";
-      let index = 0;
-      let clicked = 0;
-      counter = 0;
-      workings.textContent = "";
-      resultBox.textContent = "";
+      clearAll();
       return;
     }
     resultBox.textContent = result;
